8e81cf4d6ea0411b85ecc2e342e05253feb49d11
Eli Vergara <112268813+elivergara@users.noreply.github.com>
2024-10-04 15:58:53 -0700

    study cont

---
+++ acronyms.py
@@ -0,0 +1,13 @@
+acronyms = []
+
+acronyms.append('BFN')
+acronyms.append('LOL')
+acronyms.append('IDK')
+acronyms.append('SMH')
+acronyms.append('IMHO')
+
+print(acronyms)
+
+acronyms.remove('BFN')
+
+print(acronyms)
